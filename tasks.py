21b842bd77db026cbc7f433447932b21c5507b74
RousselotVigier <rousselotvigier.a@pg.com>
2018-03-05 11:55:24 +0100

    hello world

--- tasks.py
+++ tasks.py
@@ -3,5 +3,5 @@ from microsoftbotframework import ReplyToActivity
 
 def echo_response(message):
     if message["type"]=="message":
-        ReplyToActivity(fill=message,text=message["text"]).send()
+        ReplyToActivity(fill=message,text="Hello World!").send()
         
